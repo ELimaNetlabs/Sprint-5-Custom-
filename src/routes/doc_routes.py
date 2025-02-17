54b832ebfa91eee000bb5c35f55a58dfaf7fcba0
Emiliano Lima <elima@netlabs.com.uy>
2025-02-17 12:18:13 -0300

    Se creo la vista de colaboraciones

--- src/routes/doc_routes.py
+++ src/routes/doc_routes.py
@@ -1,12 +1,14 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, jsonify, session, render_template
+from services.doc_service import get_docs_for_collab
 
 document_bp = Blueprint('document', __name__)
 
 @document_bp.route('/documents')
 def get_documents():
-    return jsonify({"message": "Lista de documentos"})
+    docs = get_docs_for_collab(session['user_id'])
+    return render_template('docs.html', documents=docs)
 
-@document_bp.route('/documents/<int:doc_id>')
-def update_document(doc_id):
-    data = request.json
-    return jsonify({"message": f"Documento {doc_id} actualizado"})
+@document_bp.route('/create')
+def create_doc():
+    
+    return 
--- src/services/doc_service.py
+++ src/services/doc_service.py
@@ -1,5 +1,5 @@
 from database.db_postgresql import db
-from models.document import Document
+from models.document import Document, document_collaborators
 from models.user import User  
 
 def get_all_docs():
@@ -21,6 +21,26 @@ def get_docs_by_creator(creator_id):
 def get_docs_collab(user_id):
     user = User.query.get(user_id)
     return user.documents if user else []
+
+
+def get_docs_for_collab(user_id):
+    # Subquery para documentos donde el usuario es creador
+    created_docs = db.session.query(Document.doc_id).filter(Document.creator_id == user_id)
+
+    # Subquery para documentos donde el usuario es colaborador
+    collaborated_docs = db.session.query(document_collaborators.c.doc_id).filter(document_collaborators.c.user_id == user_id)
+
+    # Consulta principal: excluir documentos donde el usuario es creador o colaborador
+    docs = db.session.query(Document).filter(
+        ~Document.doc_id.in_(created_docs),  # No ser creador
+        ~Document.doc_id.in_(collaborated_docs)  # No ser colaborador
+    ).all()
+
+    return docs
+
+
+
+
     
 
 
